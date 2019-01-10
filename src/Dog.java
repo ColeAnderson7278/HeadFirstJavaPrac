1dfc761d0cedfacdc769a74e446f15af18f9d944
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-10 13:17:17 -0600

    Created my main Project class and my first example class.
    I went ahead and created a project class that I could use for all of
    the example classes in the book. The other class,Dog.java, is the first
    example I worked on just to create a simple dog that had a name, size,
    and breed. The dog also would have a method bark.

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+//    public Dog(int size, String breed, String name){
+//        this.size = size;
+//        this.breed = breed;
+//        this.name = name;
+//    }
+
+    public String bark(){
+        return "Bark! Bark!";
+    }
+}
